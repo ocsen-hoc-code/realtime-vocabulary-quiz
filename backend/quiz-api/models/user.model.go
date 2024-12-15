af398bdc24f745e24ba09599a0493870d95133e7
Dawn Do <binhminh.freelancer2024@gmail.com>
2024-12-15 12:51:51 +0700

    Forgot add uuid for user

--- backend/quiz-api/models/user.model.go
+++ backend/quiz-api/models/user.model.go
@@ -1,13 +1,26 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
+	UUID      string    `gorm:"type:char(36);uniqueIndex" json:"uuid"`
 	Username  string    `gorm:"unique;not null" json:"username"`
 	FullName  string    `gorm:"unique;not null" json:"fullname"`
 	Password  string    `gorm:"not null" json:"password"`
 	IsAdmin   bool      `gorm:"default:false" json:"is_admin"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	if u.UUID == "" {
+		u.UUID = uuid.New().String()
+	}
+	return
 }
